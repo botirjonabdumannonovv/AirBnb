0f12d5b39b63c4c0fc920b18d2b1e406ed0502e1
Botirjon <thedavlativich@gmail.com>
2023-12-14 14:57:12 +0500

    add  add cache model, cache settings and cache broker

---
+++ AirBnb/AirBnb.ServerApp/AirBnb.ServerApp.Persistence/Caching/Brokers/ICacheBroker.cs
@@ -0,0 +1,17 @@
+﻿using AirBnb.ServerApp.Domain.Common.Caching;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace AirBnb.ServerApp.Persistence.Caching.Brokers;
+
+public interface ICacheBroker
+{
+    ValueTask<T?> GetAsync<T>(string key);
+
+    ValueTask<bool> TryGetAsync<T>(string key, out T? value);
+
+    ValueTask<T?> GetOrSetAsync<T>(string key, Func<Task<T>> valueFactory, CacheEntryOptions? entryOptions = default);
+
+    ValueTask SetAsync<T>(string key, T value, CacheEntryOptions? entryOptions = default);
+
+    ValueTask DeleteAsync(string key);
+}
