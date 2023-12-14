40de97e29ec95b2c5ae782f060f71fe09c834b4b
Botirjon <thedavlativich@gmail.com>
2023-12-14 13:50:21 +0500

    add csproj

--- AirBnb/AirBnb.ServerApp/AirBnb.ServerApp.Api/obj/Debug/net8.0/AirBnb.ServerApp.Api.AssemblyInfo.cs
+++ AirBnb/AirBnb.ServerApp/AirBnb.ServerApp.Api/obj/Debug/net8.0/AirBnb.ServerApp.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AirBnb.ServerApp.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc08899d94446f3b4aed381933bdb088d88cb295")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AirBnb.ServerApp.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AirBnb.ServerApp.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
