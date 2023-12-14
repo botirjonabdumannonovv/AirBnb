8fb58eb3e90c093876cb317a57abe8126d28675d
Botirjon <thedavlativich@gmail.com>
2023-12-14 13:47:48 +0500

    add appdbcontext

---
+++ AirBnb/AirBnb.ServerApp/AirBnb.ServerApp.Persistence/DataContexts/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using AirBnb.ServerApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AirBnb.ServerApp.Persistence.DataContexts;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Location> Locations => Set<Location>();
+
+    public DbSet<LocationCategory> LocationCategories => Set<LocationCategory>();
+
+    public AppDbContext(DbContextOptions<AppDbContext>options):base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
