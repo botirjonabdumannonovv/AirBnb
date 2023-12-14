952c47a6f9fd749f96382f31a879bffc8934d852
Botirjon <thedavlativich@gmail.com>
2023-12-14 13:45:19 +0500

    add location and location category entity

---
+++ AirBnb/AirBnb.ServerApp/AirBnb.ServerApp.Domain/Entities/Location.cs
@@ -0,0 +1,14 @@
+﻿using AirBnb.ServerApp.Domain.Common.Entities;
+
+namespace AirBnb.ServerApp.Domain.Entities;
+
+public class Location : SoftDeletedEntity 
+{
+    public string? ImageUrl { get; set; }
+    
+    public string? Name { get; set; }
+    
+    public int? BuiltYear { get; set; }
+    
+    public int? PricePerNight { get; set; }
+}
---
+++ AirBnb/AirBnb.ServerApp/AirBnb.ServerApp.Domain/Entities/LocationCategory.cs
@@ -0,0 +1,10 @@
+﻿using AirBnb.ServerApp.Domain.Common.Entities;
+
+namespace AirBnb.ServerApp.Domain.Entities;
+
+public class LocationCategory : SoftDeletedEntity
+{
+    public string? Name { get; set; }
+    
+    public string? ImageUrl { get; set; }
+}
